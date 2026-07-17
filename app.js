790cdd851a454b1c7969b1a0a5c7b8a8860bea67
Dhandar <dhandarsushmita@gmail.com>
2026-07-17 17:12:27 +0530

    Validate  Schema

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const ejsMate = require("ejs-mate") ;
 const wrapAsync = require("./utils/wrapAsync.js") ;
 const ExpressError = require("./utils/ExpressError.js");
 const Joi = require('joi');
-const { listingSchema } = require("./schema")
+const { listingSchema } = require("./schema.js")
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust" ;
 main()
@@ -53,6 +53,16 @@ app.get("/",(req,res) =>{
     res.send("Hi , I AM Root...!")
 });
 
+const validateListing = (req,res,next) =>{
+    let {error} = listingSchema.validate(req.body) ;
+    if(error){
+        let errMsg = error.details.map((el) => el.message).join(",") ; 
+        throw new ExpressError(400,errMsg);
+    }else{
+        next();
+    }
+}
+
 // Index Route
 app.get(
     "/Listings",
@@ -82,11 +92,14 @@ app.get(
 // Create Route 
 app.post(
     "/Listings",
+    validateListing , 
     wrapAsync(async(req,res,next) =>{
         let result = listingSchema.validate(req.body) ;
         console.log(result) ;
+        if(result.error){
+            throw new ExpressError(400,result.error);
+        }
         const  newListing = new Listing(req.body.listing) ;
-        
         await newListing.save();
         res.redirect("/Listings") ;
     })
@@ -104,6 +117,7 @@ app.get(
 // Update route
 app.put(
     "/Listings/:id",
+    validateListing ,
     wrapAsync(async(req,res) =>{
     let {id} = req.params ;
     await Listing.findByIdAndUpdate(id,{...req.body.listing})  ;
@@ -131,7 +145,7 @@ app.use((req,res,next) =>{
 
 app.use((err, req, res, next) => {
     let { statuscode = 500, message = "Something went wrong" } = err;
-    res.render("error.ejs",{ err })
+    res.render("error.ejs",{ message })
     // res.status(statuscode).send(message);
 });
 
